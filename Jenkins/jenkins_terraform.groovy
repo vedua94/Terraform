f7ebe9c48c1f683a11556a4d621b01591ecdfbd7
ak3325 <85331258+ak3325@users.noreply.github.com>
2022-12-30 13:12:21 +0530

    added jenkins file

---
+++ Jenkins/jenkins_terraform.groovy
@@ -0,0 +1,34 @@
+pipeline {
+  agent any
+
+  parameters {
+    choice(name: 'ENV', choices: ['sb'], description: 'Select the environment to deploy into.')
+    choice(name: 'TYPE', choices: ['cluster', 'customer'], description: 'Select whether this is a cluster deployment or a customer deployment.')
+    string(name: 'CLUSTERNAME', defaultValue: '', description: 'Name of the cluster to run')
+    string(name: 'CUSTOMERNAME', defaultValue: '', description: 'Name of the customer to run')
+    choice(name: 'ACTION', choices: ['plan', 'apply', 'destroy'], description: 'Run terraform plan or terraform apply')
+  }
+  stages {
+        stage('copy_credential') {
+            steps {
+                withCredentials([file(credentialsId: "gcloud_cred", variable: 'GC_KEY')]) {
+                    sh """
+                    cp $GC_KEY ${WORKSPACE}/cred.json
+                    """
+                }   
+            }
+        }
+        stage('terraform_task'){
+            steps {
+                sh """
+                terraform init
+                terraform plan
+                """
+                input(message: 'Click "proceed" to approve the above Terraform plan')
+                """
+                terraform apply --auto-approve
+                """
+            }
+        }
+   }    
+}
