9d6315251b61316e10c07d078533d8ac3cb0030f
ak3325 <85331258+ak3325@users.noreply.github.com>
2022-12-30 13:45:10 +0530

    modified provider

--- jenkins_terraform.groovy
+++ jenkins_terraform.groovy
@@ -14,6 +14,7 @@ pipeline {
                 withCredentials([file(credentialsId: "gcloud_cred", variable: 'GC_KEY')]) {
                     sh """
                     cp $GC_KEY ${WORKSPACE}/cred.json
+                    chmod 755 cred.json
                     """
                 }   
             }
