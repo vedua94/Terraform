d95743f6c9acf98725490a160e7d9f28431f8b05
ak3325 <85331258+ak3325@users.noreply.github.com>
2023-01-02 22:43:10 +0530

    new Task

---
+++ gcp_disk/jenkins_disk.groovy
@@ -0,0 +1,37 @@
+pipeline { 
+  agent any
+  options {
+        ansiColor('xterm')
+  }
+  parameters {
+    
+    choice(name: 'ProjectID', choices: ['seraphic-ripple-372512', 'terraform-3325'], description: 'Select Project ID.')
+    string(name: 'Zone', choices: ['us-central1-a', 'us-central1-b'], description: 'Select zone.')
+    string(name: 'Diskname', defaultValue: '', description: 'Name of the Disk')
+    choice(name: 'Disksize', defaultValue: '', description: 'Name of the Disk')
+  }
+  stages {
+        stage('copy_credential') {
+            steps {
+                withCredentials([file(credentialsId: "gcloud_cred", variable: 'GC_KEY')]) {
+                    sh """
+                    cp $GC_KEY ${WORKSPACE}/gcp_disk/cred.json
+                    """
+                }   
+            }
+        }
+        stage('execute_playbook')
+             steps{
+             sh """
+             cd ${WORKSPACE}/gcp_disk/
+             sudo ansible-playbook update_disk.yaml -e "disk_project=${ProjectID} disk_zone=${Zone} disk_name=${Diskname} disk_size=${Disksize}"
+             """
+             } 
+  }               
+  post {
+        always {
+            echo '###### cleaning WorkSpace #######'
+            cleanWs notFailBuild: true, patterns: [[pattern: '**/creds.json', type: 'INCLUDE']]
+        }
+    }
+}     
