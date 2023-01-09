42fcfde8bd3fc5109483c0964c2544ae0048d5d2
ak3325 <85331258+ak3325@users.noreply.github.com>
2023-01-09 19:54:47 +0530

    new task

---
+++ variable_test/variable.groovy
@@ -0,0 +1,23 @@
+pipeline { 
+  agent any
+  options {
+        ansiColor('xterm')
+  }
+  parameters {
+    
+    choice(name: 'ProjectID', choices: ['seraphic-ripple-372512', 'terraform-3325'], description: 'Select Project ID.')
+    string(name: 'Diskname', defaultValue: '', description: 'Name of the Disk')
+  }
+  stages {
+        
+        stage('execute_playbook'){
+             steps{
+             sh """
+             chmod 755 task.sh
+             bash task.sh
+             """
+             } 
+       }
+  }                 
+      
+}    
