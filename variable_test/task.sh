876b3b580f0c15400caa06f1ef53bffd711ea72e
ak3325 <85331258+ak3325@users.noreply.github.com>
2023-01-11 11:54:07 +0530

    new task12

--- variable_test/task.sh
+++ variable_test/task.sh
@@ -3,6 +3,10 @@
 echo ${WORKSPACE}
 echo ${ProjectID}
 echo ${Diskname}
+was= `echo ${Diskname} | awk -F, '{ print $1 }'`
+web= `echo ${Diskname} | awk -F, '{ print $2 }'`
+echo ${was}
+echo ${web}
 
 
 
